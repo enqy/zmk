06b4267a95773a2bdd809b490d36f44e8bd0e300
Woze Parrot <wozeparrot@gmail.com>
2023-05-06 22:56:39 -0400

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "zmk dev flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            ccache
+            cmake
+            dfu-util
+            dtc
+            gcc-arm-embedded
+            ninja
+
+            (python3.withPackages (ps:
+              with ps; [
+                pyelftools
+                pyyaml
+                canopen
+                packaging
+                progress
+                anytree
+                intelhex
+                west
+              ]))
+          ];
+
+          ZEPHYR_TOOLCHAIN_VARIANT = "gnuarmemb";
+          GNUARMEMB_TOOLCHAIN_PATH = pkgs.gcc-arm-embedded;
+        };
+      }
+    );
+}
